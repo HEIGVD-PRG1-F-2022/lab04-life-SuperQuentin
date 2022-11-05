8ed2be74045a2d622d83f46b6de375c093f19b72
SuperQuentin <quentin.aellen@infrack.ch>
2022-11-05 22:43:06 +0100

    feat: 🎸 add util to wrap value between range

--- include/util.h
+++ include/util.h
@@ -8,11 +8,15 @@
 #define LIFE_UTIL_H
 
 #include <string>
+#include <vector>
 
 #include "./terminal.h"
 
 void wait(int milliseconds);
 
 std::string getColorsCodeStr(TerminalColors color);
+
+int wrap(int value, int min, int max);
+int wrap(int value, std::vector<std::string> vector);
 
 #endif //LIFE_UTIL_H
--- src/util.cpp
+++ src/util.cpp
@@ -19,3 +19,22 @@ string getColorsCodeStr(TerminalColors color){
     return to_string((int) color);
 }
 
+int wrap(int value, int min, int max){
+    int wrapSize = max - min + 1;
+
+    if(value < min) {
+        value += wrapSize * ((min - value)/ wrapSize + 1);
+    }
+
+    return min + (value - min) % wrapSize;
+}
+
+int wrap(int value, vector<string> vector) {
+    int wrapSize = vector.size();
+
+    if(value < 0) {
+        value += wrapSize * ((value)/ wrapSize + 1);
+    }
+
+    return (value) % wrapSize;
+}
