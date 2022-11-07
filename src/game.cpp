d27f4b52252f0a9aa54d1480fed5e9060d5f6cc0
SuperQuentin <quentin.aellen@infrack.ch>
2022-11-07 18:10:01 +0100

    feat: 🎸 add input managment to game board

--- src/game.cpp
+++ src/game.cpp
@@ -27,23 +27,44 @@ void startGame() {
 }
 
 void gameLoop() {
-    const int LIFEBOARD_WIDTH = 9;
-    const int LIFEBOARD_HEIGHT = 9;
+    int width = 21, height = 21;
+    static bool firstStart = true;
 
-    vector<vector<Cell>> lifeBoard(vector(LIFEBOARD_WIDTH, vector<Cell>(LIFEBOARD_HEIGHT)));
 
-    // TODO: delete later
-    lifeBoard[2][2] = Cell::Alive;
-    lifeBoard[3][3] = Cell::Alive;
-    lifeBoard[4][1] = Cell::Alive;
-    lifeBoard[4][2] = Cell::Alive;
-    lifeBoard[4][3] = Cell::Alive;
+    static vector<vector<Cell>> lifeBoard(vector(width, vector<Cell>(height)));
 
-    while (1) {
-        draw(lifeBoard);
-        processEvolution(lifeBoard);
-        processLifeCycle(lifeBoard);
-        wait(500);
+    if(firstStart) {
+        // TODO: delete later
+        lifeBoard[2][2] = Cell::Alive;
+        lifeBoard[3][3] = Cell::Alive;
+        lifeBoard[4][1] = Cell::Alive;
+        lifeBoard[4][2] = Cell::Alive;
+        lifeBoard[4][3] = Cell::Alive;
+    }
+
+    char input;
+    bool shouldUpdateGame = true;
+
+    while (true) {
+        firstStart = false;
+        if (input != '\000') {
+            switch (input) {
+                case 'q':
+                    clearTerminal();
+                    return;
+                default:
+                    shouldUpdateGame = true;
+            }
+        }
+
+        if(shouldUpdateGame) {
+            draw(lifeBoard);
+            processEvolution(lifeBoard);
+            processLifeCycle(lifeBoard);
+            wait(50);
+        }
+
+        input = getKeyPressDown();
     }
 }
 
